755fceb3c983a0000985cc769e677c42b5280a0b
“sloanal” <andrew@integrate.co>
2026-01-18 22:42:49 -0800

    Remove dark mode configuration from Tailwind setup to streamline styling options and focus on light mode support.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'tailwindcss'
 import tailwindcssAnimate from 'tailwindcss-animate'
 
 const config: Config = {
-  darkMode: false,
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
